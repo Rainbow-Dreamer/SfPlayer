e26b1b23d057b6359e2e46cee84254d0ce71123f
Rainbow <2180502841@qq.com>
2023-02-16 23:16:18 +0800

    Update player.py

--- packages/player.py
+++ packages/player.py
@@ -3,12 +3,7 @@ import os
 import sys
 
 sys.path.append('packages')
-import pygame
-import py
-import pydub
-import fractions
 import sf2_loader as rs
-from ast import literal_eval
 import textwrap
 import mido_fix as mido
 import random
